474d3def7bbd371a6da4a35b6bedd9482ab02407
morfuin <morfuin@gmail.com>
2018-10-04 15:12:52 +1000

    removing dbkey

--- Mount_Farmer/config_cosmos_db.py
+++ Mount_Farmer/config_cosmos_db.py
@@ -1,8 +1,8 @@
 CSRF_ENABLED = True
-SECRET_KEY = 'really-secret-key-you-could-probs-never-guess'
+SECRET_KEY = ''
 
 COSMOSDB_HOST = 'https://mount-farmer.documents.azure.com:443/'
-COSMOSDB_KEY = 'rB3EFNF2QicQMMauFhkBhlL0bNNZFjB9A8CyWkmUOIBYL99Toszz857mBqLh0j32KfVQ0Hj7GCQgGuSa8vMG4Q=='
+COSMOSDB_KEY = 'Key ending in=='
 #COSMOSDB_HOST = 'https://localhost:8081'
 #COSMOSDB_KEY = 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
 
